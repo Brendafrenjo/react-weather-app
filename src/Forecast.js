47aca347bdfb895448c22c8809f0ae98f16498cf
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-07-09 07:32:45 +0300

    Added the forecast section

--- src/Forecast.js
+++ src/Forecast.js
@@ -2,20 +2,32 @@ import React from "react";
 import "./Forecast.css";
 
 export default function Forecast() {
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"]
   return (
     <div className="Forecast">
       <div class="weather-forecast">
-        <div>
-          <div>
-            <span className="forecast-content">Sun</span>
-            <br />
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="Weather Icon"
-              className="icon mb-3 mt-3"
-            ></img>{" "}
-            <br /> <span className="forecast-content"> 25° 15°</span>
-          </div>
+              <div className="row">
+              {days.map(function (day, index) {
+                  return (
+                    <div className="col-2">
+                          <div className="forecast-content" key={index}>{ day}</div>
+                      <img
+                        src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                        alt="Weather Icon"
+                        className="icon mb-3 mt-3"
+                      ></img>{" "}
+                      <div className="forecast-content">
+                        {" "}
+                        <span class="weather-forecast-temperature-max">
+                          25°
+                        </span>{" "}
+                        <span class="weather-forecast-temperature-min">
+                          15°
+                        </span>
+                      </div>
+                    </div>
+                  );
+              })}    
         </div>
       </div>
     </div>
