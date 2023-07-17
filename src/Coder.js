c5064e4040b78b7fa967081a9a8b262bf817e09e
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-07-17 04:46:04 +0300

    Update Coder.js

--- src/Coder.js
+++ src/Coder.js
@@ -5,15 +5,23 @@ export default function Corder() {
   return (
     <small>
       <div className="Corder mt-3">
+        <span class="text-white">This project was coded by Brenda Omondi and is </span>
         <a
           href="https://github.com/Brendafrenjo/react-weather-app"
           target="_blank"
           rel="noreferrer"
           className="source-code text-white"
         >
-          Open-source code
+          open-sourced on GitHub
         </a>
-        <span className="coded-by text-white"> by Brenda Omondi</span>
+        <span className="coded-by text-white">
+          {" "}
+          and {" "}
+        </span>
+
+        <a href="https://heroic-pasca-a7d234.netlify.app/" class="text-white">
+        hosted on Netlify
+        </a>
       </div>
     </small>
   );
