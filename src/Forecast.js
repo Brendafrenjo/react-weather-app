73db37a2817d15ea04d914fe4e9806ebd4d9c901
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-07-26 21:05:23 +0300

    Updated Forecast.js

--- src/Forecast.js
+++ src/Forecast.js
@@ -23,7 +23,7 @@ export default function Forecast(props) {
         <div className="weather-forecast">
           <div className="row">
             {forecast.map(function (dailyForecast, index) {
-              if (index < 5) {
+              if (index < 6) {
                 return (
                   <div className="col" key={index}>
                     <ForecastDay data={dailyForecast} />
